6f11702dbd39e7d9e890f8c34359175f794fd89f
tomoyaki <tomoyaki74@gmail.com>
2021-08-07 23:30:25 +0900

    Style MemoList Screen

--- App.jsx
+++ App.jsx
@@ -12,37 +12,40 @@ export default function App() {
       </View>
 
       <View>
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2021年8月7日 12:30</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2021年8月7日 12:30</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
-        <View>
+
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2021年8月7日 12:30</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2021年8月7日 12:30</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
-        <View>
+
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2021年8月7日 12:30</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2021年8月7日 12:30</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
+
       </View>
 
-      <View>
-        <Text>+</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
     </View>
   );
@@ -74,5 +77,45 @@ const styles = StyleSheet.create({
     lineHeight: 32,
     color: '#ffffff',
     fontWeight: 'bold',
+  },
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.15)',
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 40,
   },
 });
