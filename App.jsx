9e968162e71e6d94648351a2f0390ff52b0a2753
tomoyaki <tomoyaki74@gmail.com>
2021-08-15 09:02:03 +0900

    Install @expo/vector-icons

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default function App() {
   return (
-    <MemoDetailScreen />
+    <MemoListScreen />
   );
 }
