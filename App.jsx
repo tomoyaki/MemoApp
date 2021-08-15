970d9ca3658252cfb6057e6bc84972d68cadb83f
tomoyaki <tomoyaki74@gmail.com>
2021-08-15 09:38:05 +0900

    Create MemoEditScreen

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    <MemoEditScreen />
   );
 }
