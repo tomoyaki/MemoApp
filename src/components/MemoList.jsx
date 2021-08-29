51ea9560f6c14e876c6c7cb35dcc73b3219be93a
tomoyaki <tomoyaki74@gmail.com>
2021-08-29 16:21:14 +0900

    Translate error code

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -8,10 +8,34 @@ import {
   shape, string, instanceOf, arrayOf,
 } from 'prop-types';
 import { dateToString } from '../utils';
+import firebase from 'firebase';
 
 export default function MemoList(props) {
   const { memos } = props;
   const navigation = useNavigation();
+
+  function deleteMemo(id) {
+    const { currentUser } = firebase.auth();
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+      Alert.alert('メモを削除します', 'よろしいですか？', [
+        {
+          text: 'キャンセル',
+          onPress: () => {},
+        },
+        {
+          text: '削除する',
+          style: 'destructive',
+          onPress: () => {
+            ref.delete().catch(() => {
+              Alert.alert('削除に失敗しました');
+            });
+          },
+        },
+      ]);
+    }
+  }
 
   function renderItem({ item }) {
     return (
@@ -25,7 +49,7 @@ export default function MemoList(props) {
         </View>
         <TouchableOpacity
           style={styles.memoDelete}
-          onPress={() => { Alert.alert('Are you shure?'); }}
+          onPress={() => { deleteMemo(item.id); }}
         >
           <Feather name="x" size={16} color="#B0B0B0" />
         </TouchableOpacity>
